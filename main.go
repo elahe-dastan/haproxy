9cae54186fcf373c6bdd9f568efa334f4b90b69c
Elahe Dastan <elahe.dstn@gmail.com>
2020-08-08 18:22:05 +0430

    Run endpoint

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"time"
+)
+
 func main() {
-	e := echo.New
+	e := echo.New()
+
+	e.GET("/", handler)
+	
+}
+
+func handler(c echo.Context) error {
+	time.Sleep(5 * time.Second)
+	fmt.Println("hi")
+
+	return nil
 }
